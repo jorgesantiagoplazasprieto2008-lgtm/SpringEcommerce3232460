0411fb6368a8c04d11414ecb926bac6047bec36f
JorgePlazas <jorgesantiagoplazasprieto2008@gmail.com>
2025-11-07 13:56:07 -0500

    v0.3_creacionInterfacesPaqueteRepository_Sagi_07112025

--- src/main/java/com/sena/springecommerce/model/DetalleOrden.java
+++ src/main/java/com/sena/springecommerce/model/DetalleOrden.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 @Table(name = "detalles")
 public class DetalleOrden {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // anotacion JPA
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Anotación JPA
 	private Integer id;
 	private String nombres;
 	private Double cantidad;
